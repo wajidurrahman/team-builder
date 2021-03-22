2cd1955b3e299fad244e7f9abf50dda3f9dec05d
Wajidur Rahman <wrfuad0@gmail.com>
2021-03-22 17:25:09 +0600

    Where to declare event handler and make arrow function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import User from './component/User/User.js';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [team, setTeam] = useState([]);
+  useEffect( () => {
+    fetch('https://randomuser.me/api/?results=15')
+    .then(res => res.json())
+    .then(data => setUsers(data.results))
+  }, [])
+  const addMember = (name) => {
+      setTeam([...team, name])
+  }
+// const add = (x, y) => x + y; example : call function
+// const addWrapper = () => add(3,5);
+// const total = addWrapper;
+// console.log(total);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1>Team Builder</h1>
+       <ul>
+         {
+           team.map((m, idx) => <li key={idx}>{m}</li>)
+         }
+       </ul>
+       {
+         users.map(user => <User user={user} addMember={addMember}></User>)
+       }
     </div>
   );
 }
---
+++ src/component/User/User.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const User = (props) => {
+    const {name, email, picture, website, phone} = props.user;
+    const addMember = props.addMember;
+   const [mobile, setMobile] = useState('');
+   const fullName = name.first + ' ' + name.last;
+    const userStyle ={
+        border: '2px solid red',
+        borderRadius: '10px',
+        margin: '10px',
+        padding:'10px',
+        display: 'flex',
+        width: '40%'
+    }
+    const showPhone = () => setMobile(phone);
+    return (
+        <div style={userStyle}>
+            <div style={{matginTop:'25px'}}>
+                <img src={picture.large} alt=""/>
+            </div>
+            <div style={{marginLeft:'20px'}}>
+                <h1>Name: {fullName}</h1>
+                <p>email: {email}</p>
+                <p><a target="_blank" href={website}>Learn about me</a></p>
+                <p>Phone: { mobile}</p>
+                <button onClick={showPhone}>Show Phone Number</button>
+                <button onClick={()=> addMember(fullName)}>Add me</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default User;
